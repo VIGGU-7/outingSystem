051b914466f8552d375d27e94069e4ece3bcdd02
viggu <hi@viggu.me>
2025-11-05 11:38:54 +0530

    added check auth feature to know the user details for frontend

--- backend/app/controllers/student.controller.js
+++ backend/app/controllers/student.controller.js
@@ -269,3 +269,12 @@ export const logout = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+export const checkAuth=async(req,res)=>{
+  try {
+    const user=req.user
+    return res.status(200).json(user)
+  } catch (error) {
+    console.error("Error in student checkauth controller:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
